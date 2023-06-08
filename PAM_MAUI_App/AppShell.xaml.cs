8c71f315637b699abb640395ba66787735432a05
Ana Viera <analirajonhson@gmail.com>
2023-06-07 22:00:05 -0300

    salvar e deletar

---
+++ PAM_MAUI_App/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PAM_MAUI_App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+        Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+    }
+}
---
+++ PAM_MAUI_App/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace PAM_MAUI_App;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        string novaFrase = ObterNovaFrase();
+
+        fraseLabel.Text = novaFrase;
+    }
+
+    private List<string> frases = new List<string>
+	{
+    "Hello, Rafael!",
+    "Hello, Ana!",
+	};
+
+    private string ObterNovaFrase()
+    {
+        Random random = new Random();
+        int index = random.Next(frases.Count);
+        return frases[index];
+    }
+}
+
